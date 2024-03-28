cb61c9f2672d90529bea20e363585bd9a20de33f
Alexandre Bassel <11035784+abassel@users.noreply.github.com>
2024-03-27 23:59:06 -0400

    Added cache via redis

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,29 @@
+import json
+
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 import requests
+import redis
 
 from .config import app_config, LOG
 
 
+redis_cache = redis.Redis(host='redis', port=6379, db=0)
+
+
 def get_weather(city: str):
 
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={app_config.WEATHER_API_KEY}'
+    city = city.strip().lower()
+
+    # Try to get data from redis cache
+    str_json = redis_cache.get(city)
+    if str_json:
+        json_dict = json.loads(str_json)
+        LOG.warning("Using cache")
+        return json_dict
+
+    LOG.debug(f"Reaching to openweathermap for city {city}")
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={app_config.WEATHER_API_KEY}'
 
     response = requests.get(url)
     if response.status_code != 200:
@@ -15,6 +31,7 @@ def get_weather(city: str):
 
     json_data = response.json()
 
+    redis_cache.set(city, json.dumps(json_data), app_config.CACHE_TTL_SECONDS)
     return json_data
 
 
