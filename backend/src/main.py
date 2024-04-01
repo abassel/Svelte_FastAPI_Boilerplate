ddff3f8f0be6c8d787f1f4b6896d795ffe53910d
Alexandre Bassel <11035784+abassel@users.noreply.github.com>
2024-04-01 01:54:10 -0400

    refactor main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,6 @@
 import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 import requests
 import redis
@@ -10,9 +10,58 @@ from .config import app_config, LOG
 
 redis_cache = redis.Redis(host='redis', port=6379, db=0)
 
+app = FastAPI(root_path="/api")
 
 def get_weather(city: str):
+    """
+    Fetches weather data for a given city from OpenWeatherMap API.
 
+    Args:
+        city (str): The name of the city for which weather data is requested.
+
+    Returns:
+        dict: A dictionary containing weather data for the specified city.
+    """
+
+    LOG.debug(f"Reaching to openweathermap for city {city}")
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={app_config.WEATHER_API_KEY}'
+
+    try:
+        response = requests.get(url)
+    except Exception as e:
+        LOG.exception(e)
+        # Do not expose stack trace to client
+        raise HTTPException(status_code=500, detail=f"Internal server error.")
+
+    if response.status_code != 200:
+        LOG.error(f'Error fetching weather data: {response.text}')
+        raise HTTPException(status_code=response.status_code, detail=f"Failed to fetch weather data -> {response.text}")
+
+    json_data = response.json()
+
+    return json_data
+
+
+@app.get("/", include_in_schema=False)
+async def docs_redirect():
+    """
+    Redirects the root URL to the API documentation.
+    """
+    return RedirectResponse(url="/api/docs")
+
+
+@app.get("/weather/{city}", operation_id="get_weather")
+async def weather_endpoint_get(city: str):
+    """
+    Endpoint to retrieve weather data for a specific city.
+    Will return cached data if requested city is found in Redis
+
+    Args:
+        city (str): The name of the city for which weather data is requested.
+
+    Returns:
+        dict: A dictionary containing weather data for the specified city.
+    """
     city = city.strip().lower()
 
     # Try to get data from redis cache
@@ -22,28 +71,7 @@ def get_weather(city: str):
         LOG.warning("Using cache")
         return json_dict
 
-    LOG.debug(f"Reaching to openweathermap for city {city}")
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={app_config.WEATHER_API_KEY}'
+    json_dict = get_weather(city=city)
+    redis_cache.set(city, json.dumps(json_dict), app_config.CACHE_TTL_SECONDS)
 
-    response = requests.get(url)
-    if response.status_code != 200:
-        LOG.error(f'Error fetching weather data: {response.text}')
-
-    json_data = response.json()
-
-    redis_cache.set(city, json.dumps(json_data), app_config.CACHE_TTL_SECONDS)
-    return json_data
-
-
-app = FastAPI(root_path="/api")
-
-
-@app.get("/", include_in_schema=False)
-async def docs_redirect():
-    return RedirectResponse(url="/api/docs")
-
-
-@app.get("/weather/{city}", operation_id="get_weather")
-async def weather_endpoint_get(city: str):
-    return get_weather(city=city)
-
+    return json_dict
